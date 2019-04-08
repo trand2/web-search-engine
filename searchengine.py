387d4593c013f58344205f8739d292913c909d5a
TRand2 <tylerrandolph123@gmail.com>
2019-04-08 16:18:20 -0600

    Added class NeedleStack

--- searchengine.py
+++ searchengine.py
@@ -2,12 +2,19 @@ import pprint
 import re
 from urllib.request import urlopen
 
+# 5. Now that you’ve written all the functions above,
+# create the NeedleStack class with the following “public” interface:
+# The NeedleStack constructor takes the seed URL, and the optional maximum search level (defaulted to 3),
+# and creates the index, graph, and ranks dictionaries,
+# which become attributes of a NeedleStack instance.
+# All the rest of the functions you wrote earlier become NeedleStack methods, some static, some non-static. 
 
-# class NeedleStack:
-#     def __init__(self,url,max_level=3):
-#
-#     def lookup(self,keyword):
-
+class NeedleStack:
+    def __init__(self, url, max_level=3):
+        self.url = url
+        self.max_level = max_level
+        self.index, self.graph = self.crawl()
+        self.ranks = self.compute_ranks()
 
 # 1. Read the contents of a given URL and find all the links embedded in the page.
 # Links are found inside anchor tags, for example <a href=”http://uvu.edu”>.
@@ -19,14 +26,14 @@ from urllib.request import urlopen
 # I suggest you use regular expressions to extract the links for this assignment. 
 
 
-def get_all_links(scrapeSite):
-    html = urlopen(scrapeSite)
-    htmlbytes = html.read()
-    html_str = htmlbytes.decode("utf8")
-    html.close()
-    urlset = set(
-        re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]| [! * \(\),] | (?: %[0-9a-fA-F][0-9a-fA-F]))+', html_str))
-    return urlset
+    def get_all_links(self, scrapeSite):
+        html = urlopen(scrapeSite)
+        htmlbytes = html.read()
+        html_str = htmlbytes.decode("utf8")
+        html.close()
+        urlset = set(
+            re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]| [! * \(\),] | (?: %[0-9a-fA-F][0-9a-fA-F]))+', html_str))
+        return urlset
 
 
 # The heart of the search engine is the web crawler. You pass a starting URL to seed this
@@ -42,56 +49,58 @@ def get_all_links(scrapeSite):
 # (This will be used subsequently for ranking web pages by “popularity”.)
 
 
-def crawl(seed, max_level):
-    tocrawl = [seed]
-    crawled = []
-    visited = 0
-    word_dict = dict()
-    graph = dict()
+    def crawl(self):
+        seed = self.url
+        max_level = self.max_level
+        tocrawl = [seed]
+        crawled = []
+        visited = 0
+        word_dict = dict()
+        graph = dict()
 
-    while tocrawl and visited < max_level:
-        crawlingURL = tocrawl[0]
-        if len(tocrawl) == 0:
-            break
+        while tocrawl and visited < max_level:
+            crawlingURL = tocrawl[0]
+            if len(tocrawl) == 0:
+                break
 
-        html = urlopen(crawlingURL)
-        htmlbytes = html.read()
-        html_str = htmlbytes.decode("utf8")
-        html.close()
-        text_str = re.sub('<[^<]+?>', '', html_str)
-        text_str = re.sub('[^\w\s]', '', text_str)
-        split_str = text_str.split()
+            html = urlopen(crawlingURL)
+            htmlbytes = html.read()
+            html_str = htmlbytes.decode("utf8")
+            html.close()
+            text_str = re.sub('<[^<]+?>', '', html_str)
+            text_str = re.sub('[^\w\s]', '', text_str)
+            split_str = text_str.split()
 
-        for x in split_str:
-            lowercase = x.lower()
-            try:
-                if word_dict[lowercase].__contains__(crawlingURL):
+            for x in split_str:
+                lowercase = x.lower()
+                try:
+                    if word_dict[lowercase].__contains__(crawlingURL):
+                        pass
+                    else:
+                        word_dict[lowercase].append(crawlingURL)
+                except KeyError:
+                    word_dict[lowercase] = [crawlingURL]
+
+            while split_str:
+                split_str.pop()
+
+            links = self.get_all_links(crawlingURL)
+
+            for value in links:
+                if value in tocrawl or value in crawled:
                     pass
                 else:
-                    word_dict[lowercase].append(crawlingURL)
-            except KeyError:
-                word_dict[lowercase] = [crawlingURL]
+                    tocrawl.append(value)
+                try:
+                    graph[crawlingURL].append(value)
+                except KeyError:
+                    graph[crawlingURL] = [value]
 
-        while split_str:
-            split_str.pop()
+            tocrawl.remove(crawlingURL)
+            crawled.append(crawlingURL)
+            visited += 1
 
-        links = get_all_links(crawlingURL)
-
-        for value in links:
-            if value in tocrawl or value in crawled:
-                pass
-            else:
-                tocrawl.append(value)
-            try:
-                graph[crawlingURL].append(value)
-            except KeyError:
-                graph[crawlingURL] = [value]
-
-        tocrawl.remove(crawlingURL)
-        crawled.append(crawlingURL)
-        visited += 1
-
-    return word_dict, graph
+        return word_dict, graph
 
 
 # 3. The next function, I will give you. It takes the graph created by crawl and returns a dictionary, ranks,
@@ -100,42 +109,43 @@ def crawl(seed, max_level):
 # This is the foundation of the algorithm Google uses for ranking pages.
 # If you would like to know more about it, visit http://en.wikipedia.org/wiki/PageRank.
 
+    def compute_ranks(self):
+        graph = self.graph
+        d = 0.85     # probability that surfer will bail
+        numloops = 10
 
-def compute_ranks(graph):
-    d = 0.85     # probability that surfer will bail
-    numloops = 10
-
-    ranks = {}
-    npages = len(graph)
-    for page in graph:
-        ranks[page] = 1.0 / npages
-
-    for i in range(0, numloops):
-        newranks = {}
+        ranks = {}
+        npages = len(graph)
         for page in graph:
-            newrank = (1 - d) / npages
-            for url in graph:
-                if page in graph[url]:  # this url links to “page”
-                    newrank += d*ranks[url]/len(graph[url])
-            newranks[page] = newrank
-        ranks = newranks
-    return ranks
+            ranks[page] = 1.0 / npages
+
+        for i in range(0, numloops):
+            newranks = {}
+            for page in graph:
+                newrank = (1 - d) / npages
+                for url in graph:
+                    if page in graph[url]:  # this url links to “page”
+                        newrank += d*ranks[url]/len(graph[url])
+                newranks[page] = newrank
+            ranks = newranks
+        return ranks
 
 # 4. Write a function, lookup(keyword), (which uses ranks) to return all the web pages in your index that
 # contain a particular word, sorted in descending order by their page rank (but don’t display the page rank).
 # Include no duplicates in your url results.
 
 
-def lookup(keyword):
-    keyword = keyword.lower()
-    link_list = list()
-    word_dict, graph = (crawl("http://freshsources.com/page1.html", 5))
-    for links in word_dict.get(keyword):
-        link_list.append(links)
-    ranks1 = compute_ranks(graph)
+    def lookup(self, keyword):
+        engine = NeedleStack(self.url, self.max_level)
+        keyword = keyword.lower()
+        link_list = list()
+        word_dict, graph = (engine.crawl())
+        for links in word_dict.get(keyword):
+            link_list.append(links)
+        ranks1 = engine.compute_ranks()
 
-    link_list.sort(key = lambda x: ranks1[x], reverse=True)
-    print(link_list)
+        link_list.sort(key = lambda x: ranks1[x], reverse=True)
+        return link_list
 
 
 
@@ -144,14 +154,13 @@ def lookup(keyword):
 
 
 if __name__ == '__main__':
-    # print('index:')
-    # word_dict, graph = (crawl("http://freshsources.com/page1.html", 5))
-    # pprint.pprint(word_dict)
-    # pprint.pprint(graph)
-    # pprint.pprint(compute_ranks(graph))
-    lookup("pages")
-    lookup("links")
-    lookup("you")
-    lookup("have")
-    lookup("I")
-
+    engine = NeedleStack('http://freshsources.com/page1.html',5)
+    for w in ['pages','links','you','have','I']:
+        print(w,'\n',engine.lookup(w))
+    print()
+    print('index:')
+    pprint.pprint(engine.index)
+    print()
+    print('graph:',engine.graph)
+    print()
+    print('ranks:',engine.ranks)
